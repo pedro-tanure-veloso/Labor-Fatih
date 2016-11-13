2428241908fad77648800c5873f7a05d8e7d5ae4
CamiloAlvarezU <camilo.alvarez.u@gmail.com>
2016-11-13 15:18:27 -0600

    Add files via upload

---
+++ HW1-Question1-LaborComp.f90
@@ -0,0 +1,74 @@
+SUBROUTINE qdratic_formula (a, b, c, ans_1, ans_2)
+    IMPLICIT NONE
+    double precision a, b ,c, ans_1,ans_2
+    
+    
+    
+    ans_1=((-b)+(b**(2.0)-(4.0*a*c))**.5)/(2.0*a)
+    ans_2=((-b)-(b**(2.0)-(4.0*a*c))**.5)/(2.0*a)  
+    
+    
+END SUBROUTINE qdratic_formula
+
+
+SUBROUTINE new_way (a, b, c, ans_1, ans_2)
+    IMPLICIT NONE
+    double precision a, b ,c, ans_1,ans_2,q
+    
+    if (b<0) Then
+        q=(-.5)*(b-(b**(2.0)-4.0*a*c)**.5)
+    else 
+        q=(-.5)*(b+(b**(2.0)-4.0*a*c)**.5)
+    END IF
+    
+    
+    ans_2=q/a
+    ans_1=c/q  
+    
+    
+END SUBROUTINE new_way
+
+double precision function How_Close( a, b)
+    IMPLICIT NONE
+    double precision a, b ,close_
+    
+    
+    close_=abs(a-b)
+    
+    print *,"a is " ,a
+    print *,"b is " ,b
+    print *," and they are " ,close_, "close "
+    
+    How_Close=close_
+
+end function How_Close
+
+
+program main
+
+    double precision a, b, c(8),d, Method_1(8,2), Method_2(8,2), closeness
+    LOGICAL cake
+    
+    
+    d = 10.0
+    a = 1.0
+    b = 100000.0
+    
+    do  i=0,7
+        c(i) = d**(-i-1)
+        call qdratic_formula (a,b,c(i),Method_1(i,0),Method_1(i,1))
+        call new_way (a,b,c(i),Method_2(i,0),Method_2(i,1))
+        
+        
+        closeness=How_Close(Method_2(i,0), Method_1(i,0))
+        
+        print *," now the other roots are: " 
+        
+        closeness=How_Close(Method_2(i,1), Method_1(i,1))
+        
+        
+    end  do
+
+
+
+end program main
